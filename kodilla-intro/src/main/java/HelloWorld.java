3572aae417065a975bdac6ae9494b1f07f5b2fbf
Artur Lukasiewicz <arturlukasiewicz0@gmail.com>
2022-09-11 15:46:37 +0200

    Classes

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Artur");
+    }
+    public static void main(int[] args) {
+        System.out.println(29);
+    }
+    public static void main(char[] args) {
+        System.out.println('L');
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,15 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Artur";
+        String myLastName = "Lukasiewicz";
+        int myAge = 29;
+        double myHeight = 1.75;
+        boolean male = true;
+        boolean female = false;
+        System.out.println(myName);
+        System.out.println(myLastName);
+        System.out.println(myHeight);
+        System.out.println(male);
+        System.out.println(female);
+    }
+}
