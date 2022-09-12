66d2dc3731a891449545353774733fe759901c52
Artur Lukasiewicz <arturlukasiewicz0@gmail.com>
2022-09-12 20:44:00 +0200

    LeapYear

---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,18 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2020;
+        String firstText = year+" is a leap year";
+        String secondText = year+" is not a leap year";
+
+        System.out.println(year);
+        System.out.println(firstText);
+        System.out.println(secondText);
+
+        if (year%4 == 0 && year%100 !=0 || year%400 == 0) {
+            System.out.println(firstText);
+        } else {
+            System.out.println(secondText);
+        }
+    }
+
+}
