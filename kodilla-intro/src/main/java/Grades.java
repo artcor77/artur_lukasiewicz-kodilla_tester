b2398e374f9db642577f972ddd597b15531fa036
Artur Lukasiewicz <arturlukasiewicz0@gmail.com>
2022-09-19 21:58:21 +0200

    Grades

---
+++ kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,34 @@
+public class Grades {
+    private int[] grades;
+    private int size;
+    double sum = 0;
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+
+    }
+    public void add(int value){
+        if (this.size==10){
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+    public int lastValue(){
+        int result = this.grades[this.size-1];
+        return result;
+        }
+
+    public int average() {
+        for (int i = 0; i< grades.length; i++) {
+            sum += grades[i];
+        }
+        sum/= grades.length;
+        System.out.println(sum);
+        // Sposób znaleziony w tutorialu. Nie wiem, czy odpowiedni na tym etapie.
+    }
+
+
+}
+
