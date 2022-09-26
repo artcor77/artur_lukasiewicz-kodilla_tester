ea84b9b3e5e1c85b48ffd3da85e5a70965e9451c
Artur Lukasiewicz <arturlukasiewicz0@gmail.com>
2022-09-26 12:28:00 +0200

    Application

--- kodilla-intro/src/main/java/Application2.java
+++ kodilla-intro/src/main/java/Application2.java
@@ -1,6 +1,6 @@
 public class Application2 {
     public static void main(String[] args) {
-        Application artur = new Application("Artur",30,175);
-
+        Application user = new Application("Artur",30,175);
+        user.checkUser();
     }
 }
---
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+
+public class RandomNumbers {
+    // Losowanie liczb z zakresu 0-30, aż ich suma osiągnie 5000.
+    // Zwrócenie najmniejszej i największej wylosowanej liczby.
+    // Pętla while
+
+    int minN = 0;
+    int maxN = 30;
+
+    public void setMaxN(int maxN) {
+        this.maxN = maxN;
+    }
+
+    public void setMinN(int minN) {
+        this.minN = minN;
+    }
+
+    public static void main(String[] args) {
+        int minN = 0;
+        int maxN = 0;
+        int sum = 0;
+        int max = 5000;
+        int liczba = 0;
+        Random random = new Random();
+        while (sum <= max) {
+            int number = range(0, 31);
+            sum = number + sum;
+
+            System.out.println(number + " " + sum);
+
+            }
+        }
+
+
+    static int range(int minN, int maxN) {
+        Random random = new Random();
+        int range = random.nextInt(maxN-minN);
+        return range;
+    }
+
+
+    }
+
