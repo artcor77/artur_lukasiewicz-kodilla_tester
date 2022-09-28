1da6954ab45ca864ac5a9242a5c994d76d6c5677
Artur Lukasiewicz <arturlukasiewicz0@gmail.com>
2022-09-28 20:39:18 +0200

    TMP-1 poprawki po review

--- kodilla-intro/src/main/java/com/kodilla/intro/Application.java
+++ kodilla-intro/src/main/java/com/kodilla/intro/Application.java
@@ -14,9 +14,9 @@ public class Application {
 
         if (name != null) {
             if (age > 30 && height > 160) {
-                System.out.println("com.kodilla.intro.User is older than 30 and taller than 160cm");
+                System.out.println("User is older than 30 and taller than 160cm");
             } else {
-                System.out.println("com.kodilla.intro.User is 30 (or younger) or 160cm (or shorter)");
+                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
             }
         }
     }
