b3d942c464ad1b4b70b8af8192e05179f4af7e76
Artur Lukasiewicz <arturlukasiewicz0@gmail.com>
2022-10-05 14:19:14 +0200

    3.5-KODILLA-Wprowadzenie do testów jednostkowych

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.basic_assertion;
+
+//Wewnątrz utworzonego pakietu dodaj klasę Calculator,
+// a w niej dwie metody: jedną dodającą dwie liczby, a drugą odejmującą jedną liczbę od drugiej.
+public class Calculator {
+
+    public int sum(int a, int b){
+        return a+b;
+    }
+    public int subtract(int a, int b){
+        return a-b;
+    }
+}
