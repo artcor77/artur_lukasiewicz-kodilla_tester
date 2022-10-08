34e834e3a6a1da33ab628e4ce1db7e2c0aa02e19
Artur Lukasiewicz <arturlukasiewicz0@gmail.com>
2022-10-08 20:29:10 +0200

    2.4-KOD-Cwiczenia: tablice(fix)

--- kodilla-intro/src/main/java/com/kodilla/intro/Grades.java
+++ kodilla-intro/src/main/java/com/kodilla/intro/Grades.java
@@ -3,32 +3,29 @@ package com.kodilla.intro;
 public class Grades {
         private int[] grades;
         private int size;
-
     public Grades() {
             this.grades = new int[10];
             this.size = 0;
             double sum = 0;
-
         }
-        public void add ( int value){
-            if (this.size == 10) {
+        public void add(int value){
+            if(this.size == 10) {
                 return;
             }
             this.grades[this.size] = value;
             this.size++;
         }
-        public int lastValue () {
+        public int lastValue() {
             int result = this.grades[this.size - 1];
             return result;
         }
-
-        public int average() {
+        public double average() {
             double sum = 0;
             for (int i = 0; i < grades.length; i++) {
                 sum += grades[i];
             }
             sum /= grades.length;
-            return average();
+            return sum;
         }
 }
 
