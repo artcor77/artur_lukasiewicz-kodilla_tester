b840c6bf6f770ec1049a6b186a3115ea8313767e
Artur Lukasiewicz <113117950+artcor77@users.noreply.github.com>
2022-10-09 14:49:09 +0200

    2.4-KOD-Cwiczenia: tablice-ZADANIE(fix) (#10)

--- kodilla-intro/src/main/java/com/kodilla/intro/Grades.java
+++ kodilla-intro/src/main/java/com/kodilla/intro/Grades.java
@@ -3,34 +3,29 @@ package com.kodilla.intro;
 public class Grades {
         private int[] grades;
         private int size;
-        private double sum;
-
     public Grades() {
             this.grades = new int[10];
             this.size = 0;
             double sum = 0;
-
         }
-        public void add ( int value){
-            if (this.size == 10) {
+        public void add(int value){
+            if(this.size == 10) {
                 return;
             }
             this.grades[this.size] = value;
             this.size++;
         }
-        public int lastValue () {
+        public int lastValue() {
             int result = this.grades[this.size - 1];
             return result;
         }
-
-        public int average() {
+        public double average() {
+            double sum = 0;
             for (int i = 0; i < grades.length; i++) {
                 sum += grades[i];
             }
             sum /= grades.length;
-            // Sposób znaleziony w tutorialu. Nie wiem, czy odpowiedni na tym etapie.
-            return average();
+            return sum;
         }
-
 }
 
