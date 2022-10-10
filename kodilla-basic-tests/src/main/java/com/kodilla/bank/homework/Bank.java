cb3814c7798eab4a372fbff8e8ed44a9a3781083
Artur Lukasiewicz <arturlukasiewicz0@gmail.com>
2022-10-10 16:41:29 +0200

    3.8-KOD-Testy jednostkowe: cwiczenia-ZADANIE

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
@@ -0,0 +1,13 @@
+package com.kodilla.bank.homework;
+
+// Bank ma zawierać tablicę bankomatów.
+// Metody zwracające:
+//
+// całkowity bilans ze wszystkich bankomatów,
+// liczbę transakcji związanych z wypłatą,
+// liczbę transakcji związaną z wpłatą pieniędzy,
+// średnią wartość wypłaty,
+// średnią wartość wpłaty.
+
+public class Bank {
+}
