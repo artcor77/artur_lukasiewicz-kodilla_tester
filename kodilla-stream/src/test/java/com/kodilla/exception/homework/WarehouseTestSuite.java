0db2579564f2ecd2fc358e5009fd465b74fd65af
Artur Lukasiewicz <arturlukasiewicz0@gmail.com>
2022-10-31 21:54:18 +0100

    KOD-6.5-ZADANIE

--- kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
@@ -2,7 +2,7 @@ package com.kodilla.exception.homework;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class WarehouseTestSuite {
 
@@ -13,5 +13,17 @@ class WarehouseTestSuite {
         //when
         //then
         assertThrows(OrderDoesntExistException.class, () -> warehouse.getOrder("0"));
+    }
+    @Test
+    public void testIsOrderInWarehouse() throws OrderDoesntExistException {
+        // given
+        Warehouse warehouse = new Warehouse();
+        warehouse.addOrder(new Order("123"));
+        warehouse.addOrder(new Order("456"));
+
+        // when
+        Order isOrderInWarehouse = warehouse.getOrder("123");
+        // then
+        assertEquals(isOrderInWarehouse.getNumber(), warehouse.getOrder("123").getNumber());
     }
 }
