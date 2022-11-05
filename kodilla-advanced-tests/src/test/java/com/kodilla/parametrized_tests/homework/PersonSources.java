526e2b4a0fb3e90457cb92a096a09dfd91001d86
Artur Lukasiewicz <arturlukasiewicz0@gmail.com>
2022-11-05 16:03:31 +0100

    KOD-7.3-ZADANIE

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonSources.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonSources.java
@@ -14,7 +14,16 @@ public class PersonSources {
                 Arguments.of(new Person(1.80, 72), "Normal (healthy weight)"),
                 Arguments.of(new Person(1.90, 130), "Obese Class II (Severely obese)"),
                 Arguments.of(new Person(1.75, 130), "Obese Class III (Very severely obese)"),
-                Arguments.of(new Person(1.80, 100), "Obese Class I (Moderately obese)")
+                Arguments.of(new Person(1.80, 100), "Obese Class I (Moderately obese)"),
+                Arguments.of(new Person(1.55, 30), "Very severely underweight"),
+                Arguments.of(new Person(1.60, 30), "Very severely underweight"),
+                Arguments.of(new Person(1.75, 80), "Overweight"),
+                Arguments.of(new Person(1.80, 180), "Obese Class V (Super Obese)"),
+                Arguments.of(new Person(1.90, 50), "Very severely underweight"),
+                Arguments.of(new Person(1.75, 200), "Obese Class VI (Hyper Obese)"),
+                Arguments.of(new Person(1.80, 150), "Obese Class IV (Morbidly Obese)"),
+                Arguments.of(new Person(1.60, 120), "Obese Class IV (Morbidly Obese)"),
+                Arguments.of(new Person(1.55, 38), "Severely underweight")
         );
     }
 }
