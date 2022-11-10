5829c45f178271ce5bd94b016f02ec2a561f32c5
Artur Lukasiewicz <arturlukasiewicz0@gmail.com>
2022-11-10 18:06:46 +0100

    KOD-7.4-ZADANIE

--- kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Order.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Order.java
@@ -34,4 +34,17 @@ public class Order {
                 ", userLogin='" + userLogin + '\'' +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Double.compare(order.value, value) == 0 && Objects.equals(date, order.date) && Objects.equals(userLogin, order.userLogin);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, date, userLogin);
+    }
 }
--- kodilla-advanced-tests/src/test/java/com/kodilla/execution_model/homework/ShopTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/execution_model/homework/ShopTestSuite.java
@@ -11,56 +11,85 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ShopTestSuite {
 
-    Shop shop = new Shop();
-    Order a = new Order(250, LocalDate.of(2022,11,7), "Art07");
-    Order b = new Order(50.50, LocalDate.of(2022,5,10), "Bar12");
-    Order c = new Order(2500.40, LocalDate.of(2022,2,15), "ano");
-    Order d = new Order(300, LocalDate.of(2021,1,30), "mar20");
-    Order e = new Order(800.60, LocalDate.of(2020,12,1), "bet56");
-    Order f = new Order(300, LocalDate.of(2021,1,30), "mar20");
-    Order g = new Order(800.60, LocalDate.of(2020,12,1), "bet56");
+    private static Shop initShop() {
+        Shop shop = new Shop();
+        Order lidl = new Order(250, LocalDate.of(2022, 11, 7), "Art07");
+        Order aldi = new Order(50.50, LocalDate.of(2022, 5, 10), "Bar12");
+        Order ikea = new Order(2500.40, LocalDate.of(2022, 2, 15), "ano");
+        Order biedronka = new Order(300, LocalDate.of(2021, 1, 30), "mar20");
+        Order auchan = new Order(800.60, LocalDate.of(2020, 12, 1), "bet56");
+        Order biedronka2 = new Order(300, LocalDate.of(2021, 1, 30), "mar20");
+        Order auchan2 = new Order(800.60, LocalDate.of(2020, 12, 1), "bet56");
+
+        shop.addOrder(lidl);
+        shop.addOrder(aldi);
+        shop.addOrder(ikea);
+        shop.addOrder(biedronka);
+        shop.addOrder(auchan);
+        shop.addOrder(biedronka2);
+        shop.addOrder(auchan2);
+        return shop;
+    }
+    private Shop shop;
 
     @BeforeEach
-    public void initializeShop() {
-        shop.addOrder(a);
-        shop.addOrder(b);
-        shop.addOrder(c);
-        shop.addOrder(d);
-        shop.addOrder(e);
-        shop.addOrder(f);
-        shop.addOrder(g);
+    public void init() {
+        shop = initShop();
+    }
+    @Test
+    public void shouldReturnCorrectOrdersListSize() {   //Teraz jest dobra ilosc pomimo zduplikowanych elementów w zbiorze(equals & hash)
+        assertEquals(5, shop.getSize());
     }
 
     @Test
-    public void shouldReturnCorrectOrdersListSize() {
-        assertEquals(7, shop.getSize());
-    }
-    @Test
     public void shouldReturnCorrectSumOfOrdersValue() {
-        assertEquals(5002.1, shop.sumOfOrdersValue());
+        assertEquals(3901.5, initShop().sumOfOrdersValue());
     }
+
     @Test
     public void shouldAddOrderWhenTheSameOrderAlreadyExist() {
-        shop.addOrder(a);
-        assertEquals(7, shop.getSize());
+        initShop().addOrder(new Order(250, LocalDate.of(2022, 11, 7), "Art07"));
+        assertEquals(5, initShop().getSize());
     }
+
     @Test
     public void shouldReturnCorrectOrdersFromDateRange() {
-        assertEquals(Set.of(a, b, c), shop.getOrderByDateRange(LocalDate.of(2022,1,1),
+        // given
+        Set<Order> expectedResult = Set.of(new Order(250, LocalDate.of(2022,11,7), "Art07"),
+                new Order(50.50, LocalDate.of(2022,5,10), "Bar12"),
+                new Order(2500.40, LocalDate.of(2022,2,15), "ano"));
+        // when then
+        assertEquals(expectedResult, initShop().getOrderByDateRange(LocalDate.of(2022,1,1),
                 LocalDate.of(2022,12,31)));
-        assertEquals(Set.of(), shop.getOrderByDateRange(LocalDate.of(2020,1,1),
+    }
+
+    @Test
+    public void shouldReturnCorrectOrdersFromDateRangeWhenDatesAreOutOfRange() {
+        //given
+        Set<Order> expectedResult = Set.of();
+        //when then
+        assertEquals(expectedResult, initShop().getOrderByDateRange(LocalDate.of(2020,1,1),
                 LocalDate.of(2020,11,30)));
     }
+
     @Test
     public void shouldReturnCorrectOrdersFromValueRange() {
-        assertEquals(Set.of(e, g), shop.getOrderByValueRange(800, 2500.39));
+        //given
+        Set<Order> expectedResult = Set.of(new Order(800.60, LocalDate.of(2020, 12, 1), "bet56"));
+        //when then
+        assertEquals(expectedResult, initShop().getOrderByValueRange(800, 2500.39));
     }
+
     @Test
     public void shouldReturnCorrectOrdersFromValueRangeWhenValuesAreTheSame() {
-        assertEquals(Set.of(e, g), shop.getOrderByValueRange(800.6, 800.6));
+        //given
+        Set<Order> expectedResult = Set.of(new Order(800.60, LocalDate.of(2020, 12, 1), "bet56"));
+        //when then
+        assertEquals(expectedResult, initShop().getOrderByValueRange(800.6, 800.6));
     }
+
     @Test
     public void shouldReturnCorrectOrdersFromValueRangeWhenValuesAreOutOfRange() {
-        assertEquals(Set.of(), shop.getOrderByValueRange(5000, 8000));
+        assertEquals(Set.of(), initShop().getOrderByValueRange(5000, 8000));
     }
 }
