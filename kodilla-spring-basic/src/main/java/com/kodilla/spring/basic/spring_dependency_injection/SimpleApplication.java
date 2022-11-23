02c12969725c1bb0eecf1db3559106e6eee4b206
Artur Lukasiewicz <arturlukasiewicz0@gmail.com>
2022-11-23 16:31:47 +0100

    KODILLA-9.2

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
@@ -0,0 +1,31 @@
+package com.kodilla.spring.basic.spring_dependency_injection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleApplication {
+
+    @Autowired
+    private SkypeMessageService messageService;    // 1 Do pola
+
+    public SimpleApplication(SkypeMessageService messageService) {     // 2 Przez konstruktor
+        this.messageService = messageService;
+    }
+
+    @Autowired
+    public void setMessageService(SkypeMessageService messageService) {      // 3 przez Setter
+        this.messageService = messageService;
+    }
+
+    public String processMessage(String message, String receiver) {
+        if (checkReceiver(receiver)) {
+            return this.messageService.send(message, receiver);
+        }
+        return null;
+    }
+
+    private boolean checkReceiver(String receiver) {
+        return receiver != null && !receiver.isEmpty();
+    }
+}
