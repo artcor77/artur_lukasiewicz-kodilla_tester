c5daee5df5df9aa9a82d1e76258190891ef2e576
Artur Lukasiewicz <113117950+artcor77@users.noreply.github.com>
2022-11-24 15:53:57 +0100

    KODILLA-9.3 (#59)

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
@@ -3,20 +3,13 @@ package com.kodilla.spring.basic.spring_dependency_injection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+
 @Component
 public class SimpleApplication {
 
-    @Autowired
-    private SkypeMessageService messageService;    // 1 Do pola
-
-    public SimpleApplication(SkypeMessageService messageService) {     // 2 Przez konstruktor
-        this.messageService = messageService;
-    }
-
-    @Autowired
-    public void setMessageService(SkypeMessageService messageService) {      // 3 przez Setter
-        this.messageService = messageService;
-    }
+    @Resource(name = "skypeMessageService")
+    private MessageService messageService;
 
     public String processMessage(String message, String receiver) {
         if (checkReceiver(receiver)) {
