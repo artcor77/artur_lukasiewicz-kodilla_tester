4ee332950fd1c35ebddf5ae3885778309d510a12
Artur Lukasiewicz <113117950+artcor77@users.noreply.github.com>
2022-12-10 16:48:29 +0100

    KODILLA-13.3 (#71)

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/search/StoreSearchExample.java
@@ -0,0 +1,23 @@
+package com.kodilla.selenium.search;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class StoreSearchExample {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium-drivers\\Chrome\\chromedriver.exe");  	// [1]
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://kodilla.com/pl/test/store");
+
+        WebElement inputField = driver.findElement(By.name("search")); // [4]
+        inputField.sendKeys("School");
+
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.name("search")));
+    }
+}
