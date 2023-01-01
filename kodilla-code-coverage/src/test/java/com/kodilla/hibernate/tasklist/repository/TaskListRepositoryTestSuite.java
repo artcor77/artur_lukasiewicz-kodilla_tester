9a6e97bce5ecf7840f646b87569fd4dfea21eede
Artur Lukasiewicz <113117950+artcor77@users.noreply.github.com>
2023-01-01 18:18:32 +0100

    KOD-15.4-ZADANIE (#90)

---
+++ kodilla-code-coverage/src/test/java/com/kodilla/hibernate/tasklist/repository/TaskListRepositoryTestSuite.java
@@ -0,0 +1,35 @@
+package com.kodilla.hibernate.tasklist.repository;
+
+import com.kodilla.hibernate.task.Task;
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TaskListRepositoryTestSuite {
+
+    @Autowired
+    private TaskListRepository taskListRepository;
+    private static final String LISTNAME = "To Do";
+    private static final String DESCRIPTION = "To Do List";
+
+    @Test
+    void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList(LISTNAME, DESCRIPTION);
+        taskListRepository.save(taskList);
+        String listName = taskList.getListName();
+        //When
+        List<TaskList> readListName = taskListRepository.findByListName(listName);
+        //Then
+        assertEquals(1, readListName.size());
+        //CleanUp
+        taskListRepository.deleteAll();
+    }
+}
