e0c6cb4fb4a30e1d6ca9c967c32411c543b9656a
Artur Lukasiewicz <arturlukasiewicz0@gmail.com>
2023-01-01 19:06:37 +0100

    KODILLA-15.5

---
+++ kodilla-code-coverage/src/main/java/com/kodilla/hibernate/task/TaskFinancialDetails.java
@@ -0,0 +1,9 @@
+package com.kodilla.hibernate.task;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TASKFINANCIALDETAILS")
+public class TaskFinancialDetails {
+}
